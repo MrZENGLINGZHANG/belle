4b90372d0d5a460fa12e14201a5af1340f0e9b5d
zenglingzhang <2932136041@qq.com>
2025-02-16 20:47:28 +0800

    install again

---
+++ fbs-gateway/src/test/java/com/sh/fbs/gateway/BizGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.fbs.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
